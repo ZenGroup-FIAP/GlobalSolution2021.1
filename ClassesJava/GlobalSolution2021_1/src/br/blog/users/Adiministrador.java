492b6422403c3a18deb454af6c27e55c4d702d65
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-06-04 09:50:44 -0300

    Classes de usuario

--- ClassesJava/GlobalSolution2021_1/src/br/blog/users/Adiministrador.java
+++ ClassesJava/GlobalSolution2021_1/src/br/blog/users/Adiministrador.java
@@ -1,5 +1,62 @@
 package br.blog.users;
 
 public class Adiministrador {
+	protected String nome;
+	protected String senha;
+	protected String email;
+	protected int id;
+	protected String tipoUsuario;
+	
+	public Adiministrador() {
+		super();
+	}
+	
+	public Adiministrador(String nome, String senha, String email, int id) {
+		super();
+		this.nome = nome;
+		this.senha = senha;
+		this.email = email;
+		this.id = id;
+		this.tipoUsuario = "Adiministrador";
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
 }
