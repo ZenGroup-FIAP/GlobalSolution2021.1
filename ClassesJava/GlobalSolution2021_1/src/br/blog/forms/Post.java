bb3e23ba929d00ed44710282ff0d0536ab78f259
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-06-05 21:29:13 -0300

    classe post

--- ClassesJava/GlobalSolution2021_1/src/br/blog/forms/Post.java
+++ ClassesJava/GlobalSolution2021_1/src/br/blog/forms/Post.java
@@ -1,5 +1,65 @@
 package br.blog.forms;
 
-public class Post {
+import java.util.Date;
 
+public class Post {
+	private int idAdm;
+	private String titulo;
+	private String conteudo;
+	private Date dataCriacao;
+	private int id;
+	
+	public Post() {
+		super();
+	}
+	
+	public Post(int idAdm, String titulo, String conteudo, Date dataCriacao, int id) {
+		super();
+		this.idAdm = idAdm;
+		this.titulo = titulo;
+		this.conteudo = conteudo;
+		this.dataCriacao = dataCriacao;
+		this.id = id;
+	}
+
+	public int getIdAdm() {
+		return idAdm;
+	}
+
+	public void setIdAdm(int idAdm) {
+		this.idAdm = idAdm;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 }
--- ClassesJava/GlobalSolution2021_1/src/br/blog/testes/TestePost.java
+++ ClassesJava/GlobalSolution2021_1/src/br/blog/testes/TestePost.java
@@ -3,7 +3,8 @@ package br.blog.testes;
 public class TestePost {
 
 	public static void main(String[] args) {
-
+		// Somente o adiministrador pode criar um post
+		
 	}
 
 }
