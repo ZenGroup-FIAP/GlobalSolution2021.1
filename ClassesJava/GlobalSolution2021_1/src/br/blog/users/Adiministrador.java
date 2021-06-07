98f79ffa7ac765318f11d2582df4dc88ce625d1b
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-06-07 09:38:34 -0300

    Classe teste da postagem finalizada

--- ClassesJava/GlobalSolution2021_1/src/br/blog/users/Adiministrador.java
+++ ClassesJava/GlobalSolution2021_1/src/br/blog/users/Adiministrador.java
@@ -1,6 +1,8 @@
 package br.blog.users;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 import br.blog.forms.Post;
@@ -12,6 +14,7 @@ public class Adiministrador {
 	protected int id;
 	protected String tipoUsuario;
 	private Scanner sc = new Scanner(System.in);
+	private List<Post> postagens = new ArrayList<Post>();
 	
 	public Adiministrador() {
 		super();
@@ -36,15 +39,32 @@ public class Adiministrador {
 		
 		
 		System.out.print("DIGITE O TÍTULO DA POSTAGEM: ");
-		String titulo = sc.next();
+		String titulo = sc.nextLine();
 		post.setTitulo(titulo);
 		
 		System.out.println("DIGITE O CONTEÚDO DA POSTAGEM: ");
 		System.out.print("-> ");
-		String conteudo = sc.next();
+		String conteudo = sc.nextLine();
 		post.setConteudo(conteudo);
 		
 		return post;
+	}
+	
+	// Método que salva post na lista de postagens
+	public void salvaPost(Post post) {
+		postagens.add(post);
+	}
+	
+	// Método que mostra todas as postagens do adiministrador
+	public void mostraPostagens() {
+		System.out.println("POSTAGENS DE " + this.nome);
+		for (Post post : postagens) {
+			System.out.println("TÍTULO: " + post.getTitulo());
+			System.out.println("CONTEÚDO: " + post.getConteudo());
+			System.out.println("DATA:" + post.getDataCriacao());
+			System.out.println("ID: " + post.getId());
+			System.out.println("---------------------------------");
+		}
 	}
 
 	public String getNome() {
