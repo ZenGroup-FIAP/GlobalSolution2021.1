23e0973807f91cb02becbab46c109e58e83b7395
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-06-07 10:05:26 -0300

    Criação do método avaliar em usuario

--- ClassesJava/GlobalSolution2021_1/src/br/blog/testes/TesteAvaliacao.java
+++ ClassesJava/GlobalSolution2021_1/src/br/blog/testes/TesteAvaliacao.java
@@ -1,9 +1,12 @@
 package br.blog.testes;
 
+import br.blog.users.Usuario;
+
 public class TesteAvaliacao {
 
 	public static void main(String[] args) {
-
+		// Somente os usuários poderão fazer as avaliações
+		Usuario user = new Usuario();
 	}
 
 }
--- ClassesJava/GlobalSolution2021_1/src/br/blog/users/Usuario.java
+++ ClassesJava/GlobalSolution2021_1/src/br/blog/users/Usuario.java
@@ -1,13 +1,14 @@
 package br.blog.users;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import br.blog.forms.Avaliacao;
 
-import java.util.ArrayList;
-
 public class Usuario extends Adiministrador {
 	private List<Avaliacao> avaliacoes = new ArrayList<Avaliacao>();
+	private Scanner sc = new Scanner(System.in);
 	
 	public Usuario() {
 		super();
@@ -21,6 +22,28 @@ public class Usuario extends Adiministrador {
 		this.email = email;
 		this.id = id;
 		this.tipoUsuario = "usuario";
+	}
+	
+	public Avaliacao avaliar(int idHotel) {
+		Avaliacao av = new Avaliacao();
+		System.out.print("DE 0 À 5 QUAL É A SUA AVALIACAO SOBRE O HOTEL: ");
+		double nota = sc.nextDouble();
+		while (nota < 0 || nota > 5) {
+			System.out.println("NOTA DE 0 À 5...");
+			System.out.print("DE 0 À 5 QUAL É A SUA AVALIACAO SOBRE O HOTEL: ");
+			nota = sc.nextDouble();
+		}
+		av.setNota(nota);
+		
+		System.out.println("DESCREVA UM COMENTÁRIO SOBRE O HOTEL");
+		System.out.print("-> ");
+		String coment = sc.nextLine();
+		av.setComentario(coment);
+		
+		av.setIdHotel(idHotel);
+		av.setIdUsuario(this.id);
+		
+		return av;
 	}
 
 	public List<Avaliacao> getAvaliacoes() {
